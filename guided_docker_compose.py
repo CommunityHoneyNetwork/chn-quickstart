979a61a978db04db6074f3be74ce5357991f1235
amerck <alexander.t.merck@gmail.com>
2019-04-23 16:10:41 -0400

    Adding options for logging and cif

--- guided_docker_compose.py
+++ guided_docker_compose.py
@@ -2,6 +2,10 @@
 import sys
 import socket
 import os
+import argparse
+import validators
+import secrets
+import string
 
 
 def make_color(color, msg):
@@ -21,6 +25,36 @@ def make_color(color, msg):
 def touch(fname, times=None):
     with open(fname, 'a'):
         os.utime(fname, times)
+
+
+def generate_password(length=32):
+
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    return password
+
+
+def check_url(url):
+    """
+    Make sure this is a real URL
+    """
+    if validators.url(url):
+        return url
+    else:
+        raise argparse.ArgumentTypeError("%s is an invalid url" % url)
+
+
+def generate_sysconfig(output_file, template_file, force_overwrite=True, **kwargs):
+    with open(template_file) as sysconfig_template_file:
+        template = sysconfig_template_file.read().format(**kwargs)
+
+    if not os.path.exists(output_file) or force_overwrite:
+        f = open(output_file, 'w')
+        f.write(template)
+        f.close()
+        print("Wrote file to %s" % output_file)
+    else:
+        sys.stderr.write("Not writing file, add -f to override\n")
 
 
 def main():
@@ -76,24 +110,93 @@ def main():
                     certificate_strategies.keys()))
             cert_strategy = None
 
+    generate_sysconfig(output_file="config/sysconfig/chnserver.sysconfig",
+                       template_file="templates/chn_server.sysconfig.template",
+                       server_base_url="https://%s" % domain,
+                       password=generate_password(),
+                       certificate_strategy=cert_strategy)
+
     # Load in template
-    f = open('docker-compose.yml.tmpl', 'r')
+    f = open('templates/docker-compose.yml.template', 'r')
     template = f.read()
     f.close()
 
-    entrypoint_list = [
-        "/code/scripts/generate_chn_sysconfig.py", "-o",
-        "/config/chnserver.sysconfig", "-s",
-        "https://%s" % domain, "-f"
-    ]
     compose = open('docker-compose.yml', 'w')
-    compose.write(template.format(entrypoint=entrypoint_list))
+    compose.write(template)
     compose.close()
     print("Wrote docker-compose.yml")
-    #   local_hp_event_logging = input('Local Honeypot Event Logging')
-    #   cif_logging_enabled = input('CIF Logging')
-    #   cif_url = input('CIF URL')
-    #   cif_api_token = input('CIF API Token')
+
+    print()
+
+    cif_logging_enabled = input(make_color("BOLD",
+                                           "Do you wish to enable logging to a remote CIFv3 server? [Y/n]: "))
+    if cif_logging_enabled.lower() == ("y" or "yes"):
+        cif_server_url = input('Please enter the URL for the remote CIFv3 server: ')
+        cif_token = input('Please enter the API token for the remote CIFv3 server: ')
+        cif_org = input('Please enter a name you wish to be associated with your organization: ')
+
+        generate_sysconfig(output_file="config/sysconfig/hpfeeds-cif.sysconfig",
+                           template_file="templates/hpfeeds-cif.sysconfig.template",
+                           cif_server_url=cif_server_url,
+                           cif_token=cif_token,
+                           cif_org=cif_org)
+        f = open('templates/docker-compose-cif.yml.template', 'r')
+        template = f.read()
+        f.close()
+
+        compose = open('docker-compose.yml', 'a')
+        compose.write(template)
+        compose.close()
+        print("Updated docker-compose.yml")
+
+    print()
+
+    local_logging_enabled = input(make_color("BOLD",
+                                             "Do you wish to enable logging to a local file? [Y/n]: "))
+    if local_logging_enabled.lower() == ("y" or "yes"):
+        log_format = None
+        while not log_format:
+            logging_formats = {
+                'splunk':
+                'Comma delimited key/value logging format for use with Splunk',
+                'json':
+                "JSON formatted log format",
+                'arcsight':
+                "Log format for use with ArcSight SIEM appliances",
+                'json_raw':
+                ("Raw JSON output from hpfeeds. More verbose that other formats,",
+                 "but also not normalized. Can generate a large amount of data.")
+            }
+            print(
+                make_color(
+                    "BOLD",
+                    "Please enter a Certificate Strategy.  This should be one of:")
+            )
+
+            print()
+            for fmt, fmt_help in logging_formats.items():
+                print("%s: %s" % (fmt, fmt_help))
+
+            log_format = input('Logging Format: ')
+            if log_format not in logging_formats.keys():
+                print()
+                sys.stderr.write(
+                    make_color(
+                        "FAIL", "You must use one of %s\n" %
+                                log_format.keys()))
+                log_format = None
+
+        generate_sysconfig(output_file="config/sysconfig/hpfeeds-logger.sysconfig",
+                           template_file="templates/hpfeeds-logger.sysconfig.template",
+                           log_format=log_format)
+        f = open('templates/docker-compose-log.yml.template', 'r')
+        template = f.read()
+        f.close()
+
+        compose = open('docker-compose.yml', 'a')
+        compose.write(template)
+        compose.close()
+        print("Updated docker-compose.yml")
     return 0
 
 
